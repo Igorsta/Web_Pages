5395b9a0e9846ac7d38e6b7a6143d7deb151669a
Igorsta <igr.staszkiewicz@gmail.com>
2025-06-10 19:18:10 +0200

    hosts

--- Task3/login_required_project/settings.py
+++ Task3/login_required_project/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['twoja-domena-render.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["web-pages-x6aw.onrender.com", 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
